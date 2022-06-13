d2a955e963cee77af63139525ceb09378f6ced87
yanzeyu <magic.yanzy@foxmail.com>
2022-06-13 23:27:40 +0800

    init

---
+++ src/main/java/com/j2lk/DailyCoding/DailyCodingApplication.java
@@ -0,0 +1,13 @@
+package com.j2lk.DailyCoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyCodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/j2lk/DailyCoding/DailyCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j2lk.DailyCoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
